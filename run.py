e1acd0d4a09e421d7c0b07384109e68053df5ae4
Zunuoh <audponu@gmail.com>
2022-12-08 21:00:37 +0000

    implemented function to edit and delete users

--- run.py
+++ run.py
@@ -13,9 +13,10 @@ try:
 except:
     print("ERROR - Cannot connect to db")
 
-users = [{"id":1,"name":"Mary"}]
+users = [{"id":1,"name":"Mary"}, {"id":2,"name":"Joseph"}, {"id":3,"name":"Anne"}]
+# users=employees
 
-nextEmployeeId = 4
+nextUserId = 4
 
 @app.route("/users", methods=["GET"])
 def get_users():
@@ -32,7 +33,52 @@ def get_user_by_id(id: int):
 def get_user(id):
     return next((e for e in users if e['id'] == id), None)
 
+def user_is_valid(user):
+    for key in user.keys():
+        if key != 'name':
+            return False
+        return True
+
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    global nextUserId
+    user = json.loads(request.data)
+    if not user_is_valid(user):
+        return jsonify({"Error":"Invalid user properties"}), 400   
+
+    user['id'] = nextUserId    
+    nextUserId += 1
+    users.append(user) 
+
+    return "", 201, {'location': f'/users/{user["id"]}'}
+
+@app.route('/users/<int:id>', methods=['PUT'])
+def update_user(id: int):
+    user = get_user(id)
+    if user is None:
+        return jsonify({'error': 'User does not exist'}), 400
+
+    updated_user = json.loads(request.data)
+    if not user_is_valid(updated_user):
+        return jsonify({'error': 'User does not exist'}), 400
+
+    user.update(updated_user)
+
+    return jsonify(user)
+
+@app.route('/users/<int:id>', methods=['DELETE'])
+def delete_user(id: int):
+    global users
+    user = get_user(id)
+    if user is None:
+        return jsonify({'error':'User does not exist'}), 404
+
+    users = [e for e in users if e['id'] != id]
+    return jsonify(user), 200
+
         
+
 
 
 # @app.route("/users", methods=["POST"])
