2e3e972fb11b261ce7a12787a6c9e0643e983b2c
Zunuoh <audponu@gmail.com>
2023-01-10 22:29:45 +0000

    created route to get all planets

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Float
 import os
+from flask_marshmallow import Marshmallow
 
 
 app = Flask(__name__)
@@ -12,6 +13,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'pl
 
 # initialize db
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 # create db
 @app.cli.command('db_create')
@@ -99,6 +101,11 @@ def url_variables(name: str, age: int):
     else:
         return jsonify(message='Welcome ' + name + ' you are old enough!')
 
+@app.route('/planets', methods=['GET'])
+def planets():
+    planets_list = Planet.query.all()
+    result = planets_schema.dump(planets_list)
+    return jsonify(result)
 
 # creating database models
 class User(db.Model):
@@ -119,6 +126,19 @@ class Planet(db.Model):
     radius = Column(Float)
     distance = Column(Float)
 
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'first_name', 'last_name', 'email', 'password')
+
+class PlanetSchema(ma.Schema):
+    class Meta:
+        fields = ('planet_id', 'planet_name', 'planet_type', 'home_star', 'mass', 'radius', 'distance')
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+planet_schema = PlanetSchema()
+planets_schema = PlanetSchema(many=True)
 
 
 
