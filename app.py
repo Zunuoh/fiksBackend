9dd4a4fc19d125f012171c3e0d68807064865f62
Zunuoh <audponu@gmail.com>
2023-01-10 21:51:30 +0000

    added records to db

--- app.py
+++ app.py
@@ -10,7 +10,65 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'planets.db')
 
+# initialize db
+db = SQLAlchemy(app)
 
+# create db
+@app.cli.command('db_create')
+def db_create():
+    db.create_all()
+    print('DATABASE CREATED')
+
+# destroy db
+@app.cli.command('db_drop')
+def db_drop():
+    db.drop_all()
+    print('DATABASE DROPPED')
+
+# seed db(add test data)
+@app.cli.command('db_seed')
+def db_seed():
+    mercury = Planet(planet_name='Mercury', 
+                    planet_type='Class D', 
+                    home_star='Sol', 
+                    mass=3.258e23, 
+                    radius=1516, 
+                    distance=35.98)
+
+    venus = Planet(planet_name='Venus', 
+                    planet_type='Class K', 
+                    home_star='Sol', 
+                    mass=4.867e24, 
+                    radius=3760, 
+                    distance=67.24e6)
+
+    earth = Planet(planet_name='Earth', 
+                    planet_type='Class M', 
+                    home_star='Sol', 
+                    mass=5.972e2, 
+                    radius=3959, 
+                    distance=92.96e6)
+    
+    # add to db as records
+    db.session.add(mercury)
+    db.session.add(venus)
+    db.session.add(earth)
+    
+
+    test_user = User(first_name='William', 
+                    last_name='Herschel', 
+                    email='test@test.com', 
+                    password='P@ssw0rd')
+
+    db.session.add(test_user)
+    # commit saves the changes
+    db.session.commit()
+    print("DATABASE SEEDED")
+    
+
+
+
+# routes
 @app.route('/')
 def hello_world():
     return jsonify(message='Hello world!')
@@ -40,6 +98,27 @@ def url_variables(name: str, age: int):
         return jsonify(message='Sorry ' + name + ' you are not old enough'), 401
     else:
         return jsonify(message='Welcome ' + name + ' you are old enough!')
+
+
+# creating database models
+class User(db.Model):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key = True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String, unique=True)
+    password= Column(String)
+
+class Planet(db.Model):
+    __tablename__ = 'planets'
+    planet_id = Column(Integer, primary_key = True)
+    planet_name = Column(String)
+    planet_type = Column(String)
+    home_star = Column(String)
+    mass = Column(Float)
+    radius = Column(Float)
+    distance = Column(Float)
+
 
 
 
